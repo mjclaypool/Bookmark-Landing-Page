00016ad74da618c378f33d846f1001eb45102dc5
mjclaypool <mallory.claypool@gmail.com>
2024-08-16 17:34:49 -0400

    Complete FAQ Accordion

--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -1,3 +1,4 @@
+import FAQAccordion from "./FAQAccordion";
 import CTA from "../UI/CTA";
 
 export default function FAQ() {
@@ -6,6 +7,7 @@ export default function FAQ() {
       <div className="text-center font-rubik max-w-[476px]">
         <h2 className="text-dark-blue text-3xl font-bold">Frequently Asked Questions</h2>
         <p className="text-grey-blue text-base my-4 lg:my-6">Here are some of our FAQs. If you have any other questions you'd like answered please feel free to email us.</p>
+        <FAQAccordion />
         <CTA color="blue">More Info</CTA>
       </div>
     </div>
---
+++ src/components/FAQAccordion.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react"
+
+export default function FAQAccordion() {
+  const [question, setQuestion] = useState(0);
+  const questions = [
+    "What is Bookmark?",
+    "How can I request a new browser?",
+    "Is there a mobile app?",
+    "What about other Chromium browsers?",
+  ]
+  const lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.";
+
+  function handleClick(index) {
+    if (index == question) {
+      setQuestion(0);
+    } else {
+      setQuestion(index);
+    }
+  }
+
+  return (
+    <div className="my-12">
+      <hr />
+      {questions.map(index => (
+        <div key={index}>
+          <div className="cursor-pointer py-4" onClick={() => handleClick(index)}>
+          <div className="flex justify-between items-center hover:text-soft-red">
+            <h3 className="text-lg">{index}</h3>
+            {question == index ?
+              <svg xmlns="http://www.w3.org/2000/svg" width="18" height="12" className="stroke-soft-red rotate-180">
+                <path fill="none" stroke="current" strokeWidth="3" d="M1 1l8 8 8-8"/>
+              </svg>
+            :
+              <svg xmlns="http://www.w3.org/2000/svg" width="18" height="12" className="stroke-soft-blue">
+                <path fill="none" stroke="current" strokeWidth="3" d="M1 1l8 8 8-8"/>
+              </svg>
+            }
+          </div>
+          {question == index && <p className="text-grey-blue text-base pt-6 pb-4 text-left">{lorem}</p>}
+          </div>
+          <hr />
+        </div>
+      ))}
+    </div>
+  )
+}
