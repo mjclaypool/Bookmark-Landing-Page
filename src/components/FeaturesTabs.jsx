abbab4a0da4677d4dfcee342608f49b2414b4b49
mjclaypool <mallory.claypool@gmail.com>
2024-08-19 14:43:52 -0400

    Add geometric background shapes

--- src/components/FeaturesTabs.jsx
+++ src/components/FeaturesTabs.jsx
@@ -4,6 +4,7 @@ import CTA from "../UI/CTA";
 import featImg1 from "../../images/illustration-features-tab-1.svg";
 import featImg2 from "../../images/illustration-features-tab-2.svg";
 import featImg3 from "../../images/illustration-features-tab-3.svg";
+import bgBlob from "../../images/bg-blob.svg";
 
 export default function FeaturesTabs() {
   const [tab, setTab] = useState("bookmarking");
@@ -87,13 +88,22 @@ export default function FeaturesTabs() {
         </div>
         <hr />
       </div>
-      <div className="flex flex-col lg:flex-row justify-center items-center gap-24 px-6 my-12 lg:px-40">
-        <div>
-          <img src={content[tab].img} alt="Feature illustration" />
+      <div className="flex flex-col lg:flex-row w-[100vw] justify-center items-center gap-24 my-12">
+        <div className="relative flex flex-col w-[100vw] lg:w-[50vw] items-center lg:items-end">
+          <img
+            src={content[tab].img}
+            alt="Feature illustration"
+            className="relative lg:max-w-[440px] max-h-[284px] object-contain px-12 lg:px-0 z-20"
+          />
+          <img
+            src={bgBlob}
+            alt="Blue geometric background shape."
+            className="absolute w-[60vw] lg:w-[45vw] h-full lg:h-[95%] left-0 -bottom-12 z-10 rotate-180"
+          />
         </div>
-        <div className="flex flex-col items-center lg:items-start text-center lg:text-start lg:w-[400px]">
+        <div className="flex flex-col items-center lg:items-start text-center max-w-[476px] lg:max-w-full lg:text-start lg:w-[50vw] lg:pr-12">
           <h3 className="text-dark-blue text-2xl lg:text-3xl font-bold">{content[tab].title}</h3>
-          <p className="text-grey-blue text-base my-4 lg:my-6">{content[tab].text}</p>
+          <p className="text-grey-blue text-base lg:max-w-[410px] my-4 lg:my-6 px-6 lg:px-0">{content[tab].text}</p>
           <div>
             <CTA color="blue">More Info</CTA>
           </div>
